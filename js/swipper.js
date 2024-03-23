8889f62fe517ac4e2c4d94e8b20bd0a3d6dacb81
osamaashraf6 <osamaashraf668@gmail.com>
2024-03-23 21:33:45 +0200

    Add the multi step form in the investment page

--- js/swipper.js
+++ js/swipper.js
@@ -1,40 +1,62 @@
-//  Start swiper one
-const swiperEl = document.querySelector("swiper-container");
-const swiper = swiperEl.swiper;
+const slidePage = document.querySelector(".slide-page");
+const nextBtnFirst = document.querySelector(".firstNext");
+const prevBtnSec = document.querySelector(".prev-1");
+const nextBtnSec = document.querySelector(".next-1");
+const prevBtnThird = document.querySelector(".prev-2");
+const nextBtnThird = document.querySelector(".next-2");
+const prevBtnFourth = document.querySelector(".prev-3");
+const submitBtn = document.querySelector(".submit");
+const progressBar = document.querySelector(".progress-bar");
+const bullet = document.querySelectorAll(".step .bullet");
+let current = 1;
 
-var appendNumber = 4;
-var prependNumber = 1;
-document
-  .querySelector(".prepend-2-slides")
-  .addEventListener("click", function (e) {
-    e.preventDefault();
-    swiper.prependSlide([
-      "<swiper-slide>Slide " + --prependNumber + "</swiper-slide>",
-      "<swiper-slide>Slide " + --prependNumber + "</swiper-slide>",
-    ]);
-  });
-document
-  .querySelector(".prepend-slide")
-  .addEventListener("click", function (e) {
-    e.preventDefault();
-    swiper.prependSlide(
-      "<swiper-slide>Slide " + --prependNumber + "</swiper-slide>"
-    );
-  });
-document.querySelector(".append-slide").addEventListener("click", function (e) {
-  e.preventDefault();
-  swiper.appendSlide(
-    "<swiper-slide>Slide " + ++appendNumber + "</swiper-slide>"
-  );
+nextBtnFirst.addEventListener("click", function (event) {
+  event.preventDefault();
+  slidePage.style.marginLeft = "-25%";
+  bullet[current - 1].classList.add("active");
+  progressBar.style.borderBottomLeftRadius = "0";
+  progressBar.style.borderTopLeftRadius = "0";
+  progressBar.style.borderLeftWidth = "0";
+  current += 1;
 });
-document
-  .querySelector(".append-2-slides")
-  .addEventListener("click", function (e) {
-    e.preventDefault();
-    swiper.appendSlide([
-      "<swiper-slide>Slide " + ++appendNumber + "</swiper-slide>",
-      "<swiper-slide>Slide " + ++appendNumber + "</swiper-slide>",
-    ]);
-  });
+nextBtnSec.addEventListener("click", function (event) {
+  event.preventDefault();
+  slidePage.style.marginLeft = "-50%";
+  bullet[current - 1].classList.add("active");
+  current += 1;
+});
+nextBtnThird.addEventListener("click", function (event) {
+  event.preventDefault();
+  slidePage.style.marginLeft = "-75%";
+  bullet[current - 1].classList.add("active");
+  current += 1;
+});
+submitBtn.addEventListener("click", function () {
+  bullet[current - 1].classList.add("active");
+  current += 1;
+  setTimeout(function () {
+    alert("Your Form Successfully Signed up");
+    location.reload();
+  }, 800);
+});
 
-//  End swiper one
+prevBtnSec.addEventListener("click", function (event) {
+  event.preventDefault();
+  slidePage.style.marginLeft = "0%";
+  bullet[current - 2].classList.remove("active");
+  progressBar.style.borderLeftWidth = "2px";
+  progressBar.style.borderRadius = "30px";
+  current -= 1;
+});
+prevBtnThird.addEventListener("click", function (event) {
+  event.preventDefault();
+  slidePage.style.marginLeft = "-25%";
+  bullet[current - 2].classList.remove("active");
+  current -= 1;
+});
+prevBtnFourth.addEventListener("click", function (event) {
+  event.preventDefault();
+  slidePage.style.marginLeft = "-50%";
+  bullet[current - 2].classList.remove("active");
+  current -= 1;
+});
